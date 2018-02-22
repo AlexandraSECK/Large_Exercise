e1b6d7d9cf9f458c3c8612679fb9a5b9e5e802d3
AlexandraSECK <35805221+AlexandraSECK@users.noreply.github.com>
2018-02-22 21:26:15 +0100

    Update

--- www/js/sql.js
+++ www/js/sql.js
@@ -1,5 +1,6 @@
-//$ip="192.168.43.230";
-$ip="192.168.102.144";
+$ip="192.168.43.230";
+//$ip="192.168.102.144";
+//$ip="10.4.2.11";
 //-------------phase 4--------------------------
 function saveData(user,address,lat,lng,info) {
   var url = "http://"+$ip+"/MIP/insertdata.php?user=" + user + "&address=" + address +
@@ -27,7 +28,6 @@ function downloadUrl(url) {
 //-------------------------phase 5-------------------------------
 function displayImages(lat,lng) {
     var url = "http://"+$ip+"/MIP/connect.php?lat=" + lat + "&lng=" + lng;
-
     downloadUrlb(url, function(data) {
         var xml = data.responseXML;
         var markers = xml.documentElement.getElementsByTagName('Listphotos');
@@ -52,3 +52,12 @@ function downloadUrlb(url, callback) {
   request.open('GET', url, true);
   request.send(null);
 }
+
+function saveImage(address,lat,lng,photo) {
+  var url = "http://"+$ip+"/MIP/insertphoto.php?address=" + address +
+  "&photo=" + photo + "&lat=" + lat + "&lng=" + lng;
+  downloadUrl(url);
+}
+
+
+
