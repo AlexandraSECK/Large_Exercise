de39ba77f0e52588bb30408ed5eb11e78c1a5b44
AlexandraSECK <35805221+AlexandraSECK@users.noreply.github.com>
2018-02-22 10:11:14 +0100

    commit fifth phase

---
+++ www/js/sql.js
@@ -0,0 +1,54 @@
+//$ip="192.168.43.230";
+$ip="192.168.102.144";
+//-------------phase 4--------------------------
+function saveData(user,address,lat,lng,info) {
+  var url = "http://"+$ip+"/MIP/insertdata.php?user=" + user + "&address=" + address +
+  "&info=" + info + "&lat=" + lat + "&lng=" + lng;
+  downloadUrl(url);
+}
+
+function downloadUrl(url) {
+  var request = window.ActiveXObject ?
+      new ActiveXObject('Microsoft.XMLHTTP') :
+      new XMLHttpRequest;
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      request.onreadystatechange = doNothing;
+    }
+  };
+  request.open('GET', url, true);
+  request.send(null);
+}
+
+
+ function doNothing () {
+}
+
+//-------------------------phase 5-------------------------------
+function displayImages(lat,lng) {
+    var url = "http://"+$ip+"/MIP/connect.php?lat=" + lat + "&lng=" + lng;
+
+    downloadUrlb(url, function(data) {
+        var xml = data.responseXML;
+        var markers = xml.documentElement.getElementsByTagName('Listphotos');
+        Array.prototype.forEach.call(markers, function(markerElem) {
+            var photo = markerElem.getAttribute('photo');
+            document.getElementById("images").innerHTML+="<img src='"+photo+"'/>";
+        });
+    });
+}
+
+
+function downloadUrlb(url, callback) {
+  var request = window.ActiveXObject ?
+  new ActiveXObject('Microsoft.XMLHTTP') :
+  new XMLHttpRequest;
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      request.onreadystatechange = doNothing;
+      callback(request, request.status);
+    }
+  };
+  request.open('GET', url, true);
+  request.send(null);
+}
